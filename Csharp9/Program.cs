5cc17fcef071a588ffb41380e042a7d82cf0d217
Guven <guven89@hotmail.com>
2020-09-18 00:04:19 +0200

    adjust demo

--- Csharp9/Program.cs
+++ Csharp9/Program.cs
@@ -14,16 +14,20 @@ namespace Csharp9
 
             var newp = p with { };
             newp.FirstName = "changed";
-
-            var product = new Product("Phone", 14);
-
-            var (name, lastname) = p;
-
-            Console.WriteLine($"{name} {lastname}");
-            Console.WriteLine($"{product.Name} {product.CategoryId}");
+            //newp.LastName = "last";
 
             Console.WriteLine(p.FirstName);
             Console.WriteLine(newp.FirstName);
+
+            #region Product
+
+            //var product = new Product("Phone", 14);
+            //Console.WriteLine($"{product.Name} {product.CategoryId}");
+
+            //var (name, lastname) = p;
+            //Console.WriteLine($"{name} {lastname}");
+
+            #endregion
 
             Console.ReadKey();
         }
@@ -32,7 +36,7 @@ namespace Csharp9
     public record Person
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string LastName { get; init; }
 
         public void Deconstruct(out string firstName, out string lastName)
         {
