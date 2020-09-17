7bc5caf3b5e74868fa7475fd17eb76ac48962d2c
Guven <guven89@hotmail.com>
2020-09-18 00:35:54 +0200

    Add next features

--- Csharp9/Program.cs
+++ Csharp9/Program.cs
@@ -19,6 +19,8 @@ namespace Csharp9
             Console.WriteLine(p.FirstName);
             Console.WriteLine(newp.FirstName);
 
+            
+
             #region Product
 
             //var product = new Product("Phone", 14);
@@ -26,6 +28,8 @@ namespace Csharp9
 
             //var (name, lastname) = p;
             //Console.WriteLine($"{name} {lastname}");
+
+            //Product pr2 = new("3", 5);
 
             #endregion
 
@@ -45,4 +49,24 @@ namespace Csharp9
     }
 
     public record Product(string Name, int CategoryId);
+
+    //#region Next 
+
+    //// Before
+    //void Insert(string s)
+    //{
+    //    if (s is null)
+    //        throw new ArgumentNullException(nameof(s));
+
+    //}
+
+    //// After
+    //void Insert(string s!)
+    //{
+
+    //}
+
+    //const string s1 = $"abc";
+
+    //#endregion
 }
