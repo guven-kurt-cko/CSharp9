3c0c55cb96fb194730d9007282d93e0a269e6494
Guven <guven89@hotmail.com>
2020-11-11 23:26:04 +0100

    edit

--- Csharp9/Program.cs
+++ Csharp9/Program.cs
@@ -16,20 +16,40 @@ namespace Csharp9
             newp.FirstName = "changed";
             //newp.LastName = "last";
 
+            if (p is not null)
+                Console.WriteLine("not null");
+
+            // another way of is not null
+            //string str = "g";
+            //if (str is object)
+            //    Console.WriteLine("string is not null");
+
             Console.WriteLine(p.FirstName);
             Console.WriteLine(newp.FirstName);
+           
 
+            //var dummy = new Dummy()
+            //{
+            //    FirstName = "Test"
+            //};
 
 
             #region Product
-
-            var product = new Product("Phone", 14);
+            // Target-typed new expressions
+            Product product = new("Parfume", 33);
             Console.WriteLine($"{product.Name} {product.CategoryId}");
 
-            var pr = new Product("first", 1) { Name = "second", CategoryId = 2 };
+            // ctor and obj-initializer
+            var pr = new Product("first", 1)
+            {
+                Name = "second",
+                CategoryId = 2
+            };
             //pr.LastName = "gege";
             Console.WriteLine(pr.Name + " " + pr.CategoryId);
 
+
+            // deconstructor
             var (name, lastname) = p;
             Console.WriteLine($"{name} {lastname}");
 
@@ -38,6 +58,20 @@ namespace Csharp9
             #endregion
 
             Console.ReadKey();
+        }
+    }
+
+    public class Dummy
+    {
+        public Dummy() { }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Dummy(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
         }
     }
 
